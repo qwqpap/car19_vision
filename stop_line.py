f5224326081ec1a734aafd5fcf76e83ff337b48e
qwqpap <74455027+qwqpap@users.noreply.github.com>
2023-10-31 13:59:39 +0800

    Halloween commit

--- stop_line.py
+++ stop_line.py
@@ -10,17 +10,25 @@ def stop_line(img):
 
     small_img = cv2.resize(img, down_points, interpolation=cv2.INTER_LINEAR)
 
+    start_pix = 230
+    end_pix = 250
+
+    small_img = small_img[start_pix:end_pix, 0:640]
+
     hsv = cv2.cvtColor(small_img, cv2.COLOR_BGR2HSV)
 
     yellow = cv2.inRange(hsv, np.array([100, 130, 50]), np.array([128, 255, 240]))
 
     all = (yellow.sum()) / 255
 
-    counts = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
     target_pix = 50
 
-    if all >= target_pix and counts >= 2:
+    if all >= target_pix :
         return True
     else:
         return False
+
+if __name__ == "__main__":
+    img = cv2.imread("stop_photo/11.jpg")
+    fina = stop_line(img)
+    print(fina)
